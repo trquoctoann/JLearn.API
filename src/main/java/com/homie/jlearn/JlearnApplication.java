ed17fcea4df2b7c714c8f53e61bff3ae4e1f02df
Quoc Toan Tran <quoctoan09072002@gmail.com>
2025-01-02 14:08:13 +0700

    initial commit

---
+++ src/main/java/com/homie/jlearn/JlearnApplication.java
@@ -0,0 +1,13 @@
+package com.homie.jlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JlearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JlearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homie/jlearn/JlearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homie.jlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JlearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
